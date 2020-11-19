d71bcd8da0cd05a4b837b39d9caca360405bb430
SolarCoder <74742542+solar-coder@users.noreply.github.com>
2020-11-19 17:28:20 -0500

    Add files via upload

---
+++ func_params.py
@@ -0,0 +1,11 @@
+# Author: SolarCoder
+# Instagram | @solarcoder
+
+# defining function
+# with a parameter named "message"
+def hello(message):
+    # using the parameter
+    print("Your message:", message)
+
+# calling the function with a message!
+hello("Hello coders!")
